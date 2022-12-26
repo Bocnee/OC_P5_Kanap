aa74d39aacceb1270e4e8ea8c07d4b7a1e7a42d2
Océane Burel <oceanebrl.pro@gmail.com>
2022-12-26 14:56:55 +0100

    Selections alert & array for the customer's selection

--- front/js/product.js
+++ front/js/product.js
@@ -40,3 +40,41 @@ function displayProductId(product) {
     colors.innerHTML += `<option value="${product.colors[color]}">${product.colors[color]}</option>`;
     };
 };
+
+// create the local storage
+function storeInLocal() {
+    var addToCart = document.getElementById("addToCart");
+    addToCart.addEventListener("click", () => {
+// colors.value & quantity.value
+        var colorV = document.querySelector("#colors").value;
+        var quantityV = document.querySelector("#quantity").value;
+// check if the color & quantity are chosen 
+        if (colorV === "" && quantityV < 1 || quantityV > 100) {
+            window.alert("Merci de séléctionner une couleur et une quantité.");
+// check if the color is chosen
+        } else if (colorV === "") {
+            window.alert("Merci de choisir une couleur.");
+            return;
+// check if a quantity is chosen
+        } else if (quantityV === 0) {
+            window.alert("Merci de choisir une quantité.");
+            return;
+// check if the quantity isn't negative
+        } else if (quantityV < 0) {
+            window.alert("La quantité ne peut pas être négative.");
+            return;
+// check if the quantity isn't more than 100
+        } else if (quantityV > 100) {
+            window.alert("La quantité maximale est de 100, merci de choisir une quantité inférieure.");
+            return;
+//  then, an array with the customer's selection
+        } else {
+            var customSelect = {
+                id: productId,
+                color: colorV,
+                quantity: quantityV,
+            };
+    })
+};
+
+storeInLocal();

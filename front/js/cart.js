21dbb28a74262646705d4bd485d935add35ff0a9
Océane Burel <oceanebrl.pro@gmail.com>
2023-01-05 20:30:03 +0100

    total price and total quantity added

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,6 +2,8 @@
 // call the local storage
 var cart = JSON.parse(localStorage.getItem("product"));
 // function if the cart is empty
+var totalQuantity = 0
+var totalPrice = 0
 function emptyCart(products) {
     if (cart === null) {
         var emptyCart = document.querySelector("#cart__items");
@@ -37,26 +39,32 @@ function displayCartProduct(cartElement, apiElement) {
     title.innerText = `Panier`
     const section = document.getElementById("cart__items");
     section.innerHTML += `
-    <article class="cart__item" data-id="${apiElement._id}" data-color="${cartElement.color}">
-    <div class="cart__item__img">
-      <img src="${apiElement.imageUrl}" alt="${apiElement.altTxt}">
-    </div>
-    <div class="cart__item__content">
-      <div class="cart__item__content__description">
-        <h2>${apiElement.name}</h2>
-        <p>${cartElement.color}</p>
-        <p>${apiElement.price * cartElement.quantity} €</p>
-      </div>
-      <div class="cart__item__content__settings">
-        <div class="cart__item__content__settings__quantity">
-          <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartElement.quantity}">
+      <article class="cart__item" data-id="${apiElement._id}" data-color="${cartElement.color}">
+        <div class="cart__item__img">
+          <img src="${apiElement.imageUrl}" alt="${apiElement.altTxt}">
         </div>
-        <div class="cart__item__content__settings__delete">
-          <p class="deleteItem">Supprimer</p>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${apiElement.name}</h2>
+            <p>${cartElement.color}</p>
+            <p>${apiElement.price} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté : </p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartElement.quantity}">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </article>
+      </article>
     `
+const tQuantity = document.getElementById("totalQuantity");
+const tPrice = document.getElementById("totalPrice");
+totalQuantity += Number(cartElement.quantity);
+totalPrice += Number(apiElement.price) * Number(cartElement.quantity); 
+tQuantity.innerText = totalQuantity;
+tPrice.innerText = totalPrice;
 }
